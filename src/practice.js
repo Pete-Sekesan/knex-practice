d11ff3828c898b7b17c39e591c6baf559fd0d7c0
Peter Sekesan <psekesan@gmail.com>
2021-02-09 16:05:10 -0500

    building queires.

--- src/practice.js
+++ src/practice.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const knex = require("knex");
 
 const knexInstance = knex({
@@ -5,10 +6,10 @@ const knexInstance = knex({
   connection: process.env.DB_URL,
 });
 
-const q1 = knexInstance("amazong_products").select("*").toQuery();
-const q2 = knexInstance.from("amazong_products").select("*").toQuery();
-
-console.log("q1:", q1);
-
-console.log("q2:", q2);
+knexInstance
+  .from("amazong_products")
+  .select("*")
+  .then((result) => {
+    console.log(result);
+  });
 console.log("knex and driver installed correctly");
